586a48ec4bdf6744cca54790ddbb113d0e141b07
abdimoh596 <107062181+abdimoh596@users.noreply.github.com>
2025-06-18 15:06:59 -0700

    fixed project structure

--- src/main/java/com/understandai/understandai/controller/RepoController.java
+++ src/main/java/com/understandai/understandai/controller/RepoController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 public class RepoController {
     @PostMapping("/analyze")
     public ResponseEntity<String> analyzeRepo(@RequestBody RepoRequest repoRequest) {
-        // Here you would typically handle the request, e.g., save the repo URL to a database
         String repoUrl = repoRequest.getRepoUrl();
         System.out.println("Received repo URL: " + repoUrl);
         // For demonstration, we just return the received URL
